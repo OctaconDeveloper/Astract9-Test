3640ff62efec2c3c36cd68e398c4d3e3cfc94ef8
OctaconDeveloper <kaakadev@gmail.com>
2020-03-13 22:40:57 +0100

    initial

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:vJ2JHOBusShPX/WmoW1zvvg699A/GE+EhflOvGRa65s='),
 
     'cipher' => 'AES-256-CBC',
 
