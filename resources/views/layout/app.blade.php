ed5f56711616325588b18392c099a13a7b313285
OctaconDeveloper <kaakadev@gmail.com>
2020-03-14 01:26:48 +0100

    modified composer.json

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,7 +13,7 @@
     <link href="{{ secure_asset('css/animate.css') }}" rel="stylesheet">
 	<link href="{{ secure_asset('css/main.css') }}" rel="stylesheet">
 	<link href="{{ secure_asset('css/responsive.css') }}" rel="stylesheet">     
-    <link rel="shortcut icon" href="{{ secure_asset('images/ico/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ secure_asset('images/home/logo.png') }}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ secure_asset('images/ico/apple-touch-icon-144-precomposed.png') }}"> 
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ secure_asset('images/ico/apple-touch-icon-114-precomposed.png') }}">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ secure_asset('images/ico/apple-touch-icon-72-precomposed.png') }}">
